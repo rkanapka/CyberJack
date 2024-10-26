84592afbf89876aba79e0479269645faeb1bf3a5
rkanapka <kanapka.rimvydas@gmail.com>
2024-10-27 01:07:08 +0300

    imp: load card images

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from time import sleep
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 import threading
 from kivy.core.audio import SoundLoader
 from kivy.clock import mainthread
@@ -26,22 +27,23 @@ class MainMenu(Screen):
 class PlayGame(Screen):
     CURRENCY = "€$"
 
+    my_card_box = None
+
     left_money = NumericProperty(1000)
     your_bet = NumericProperty(0)
-    your_hand = StringProperty("")
-    y_hand = []
+    your_hand = []
     sum_of_hand = NumericProperty(0)
 
-    dealer_hand = StringProperty("")
-    d_hand = []
-    sum_dealer = NumericProperty(0)
+    opponent_card_box = None
+    opponent_hand = []
+    sum_opponent = NumericProperty(0)
 
     status_text = ""  # winning/losing message
     current = ""  # current window
     chip_btn_text = StringProperty('')
 
     suit = ["S", "H", "C", "D"]  # Spades, Hearts, Clubs, Diamonds
-    card = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
+    card = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     deck = []
 
     def make_deck(self):
@@ -88,15 +90,19 @@ class PlayGame(Screen):
         return arg1  # sum of cards value returned here
 
     @mainthread
-    def popup(self, arg1):  # arg1 is popup message
+    def popup(self, text):
         sleep(1)
         fl = FloatLayout()
-        fl.add_widget(Label(text=arg1, pos_hint={'x': 0, 'center_y': 0.95}))
+        fl.add_widget(Label(text=text, pos_hint={'x': 0, 'center_y': 0.95}))
         popup = Popup(title="", separator_height=0, title_size=10, content=fl,
                       size_hint=(None, None), size=(300, 100),
                       auto_dismiss=False, pos_hint={'x': 0.3, 'center_y': 0.48})
-        but = Button(text="Go on", on_press=popup.dismiss, pos_hint={'x': 0.12, 'center_y': 0.48}, size_hint=(0.8, 0.2))
-        but.bind(on_press=lambda x: self.reset())
+        but = Button(
+            text="Go on",
+            pos_hint={'x': 0.12, 'center_y': 0.48},
+            size_hint=(0.8, 0.2)
+        )
+        but.bind(on_press=lambda x: self.reset(popup))
         fl.add_widget(but)
         popup.open()
 
@@ -105,11 +111,17 @@ class PlayGame(Screen):
             self.make_deck()
 
         SoundLoader.load("sounds/deal_cards.wav").play()
-        self.your_hand = self.deck[0] + " " + self.deck[1]
-        self.dealer_hand = self.deck[2] + " " + self.deck[3]
+        self.your_hand.extend(self.deck[:2])
+        my_cards_box_widget = self.load_card_images(self.your_hand)
+        if my_cards_box_widget is not None:
+            self.add_widget(my_cards_box_widget)
+        
+        self.opponent_hand.extend(self.deck[2:4])
+        opponent_cards_box_widget = self.load_card_images(self.opponent_hand, is_opponent=True)
+        if opponent_cards_box_widget is not None:
+            self.add_widget(opponent_cards_box_widget)
 
-        for i in range(4):
-            self.deck.remove(self.deck[0])  # delete dealt cards from the deck
+        del self.deck[:5]  # delete dealt cards from the deck
 
         self.ids.hit_btn.disabled = False  # enable HIT button
         self.ids.stand_btn.disabled = False  # enable STAND button
@@ -118,34 +130,86 @@ class PlayGame(Screen):
         self.ids.fifty.disabled = True  # disable 50 chip button
         self.ids.hundred.disabled = True  # disable 100 chip button
 
-        self.sum_of_hand = self.total_sum(self.sum_of_hand, self.y_hand, self.your_hand)
-        self.sum_dealer = self.total_sum(self.sum_dealer, self.d_hand, self.dealer_hand)
+        self.sum_of_hand = self.total_sum(self.your_hand)
+        self.sum_opponent = self.total_sum(self.opponent_hand)
+
+    def load_card_images(self, cards_in_hand, is_opponent=False):
+        if is_opponent:
+            self.opponent_card_box, is_cards_box_reused = self.get_or_create_card_box(
+                self.opponent_card_box, center_x=0.8, top=0.65, cards_in_hand=cards_in_hand
+            )
+            cards_box = self.opponent_card_box
+        else:
+            self.my_card_box, is_cards_box_reused = self.get_or_create_card_box(
+                self.my_card_box, center_x=0.5, top=0.45, cards_in_hand=cards_in_hand
+            )
+            cards_box = self.my_card_box
+
+        for card_name in cards_in_hand:
+            cards_box.add_widget(
+                Image(
+                    source=f"images/cards/{card_name}.jpg",
+                    size_hint_x=None,
+                    width=100
+                )
+            )
+        if is_cards_box_reused:
+            return None
+        return cards_box
+
+    @staticmethod
+    def get_or_create_card_box(existing_box, center_x, top, cards_in_hand):
+        if existing_box:
+            return existing_box, True
+        spacing = 5
+        return BoxLayout(
+            size_hint=(None, 0.2),
+            pos_hint={'center_x': center_x, 'top': top},
+            width=len(cards_in_hand) * 100 + (len(cards_in_hand) - 1) * spacing,
+            spacing=spacing
+        ), False
 
     def hit_card(self):
         SoundLoader.load("sounds/deal_card.wav").play()
-        self.your_hand += " " + self.deck[0]
+        self.your_hand.append(self.deck[0])
+
+        self.my_card_box.add_widget(
+                Image(
+                    source=f"images/cards/{self.deck[0]}.jpg",
+                    size_hint_x=None,
+                    width=100
+                )
+            )
         del self.deck[0]  # delete "first" card of the deck
 
         # sum of cards value again, because we draw 1 more card
-        self.sum_of_hand = self.total_sum(self.sum_of_hand, self.y_hand, self.your_hand)
+        self.sum_of_hand = self.total_sum(self.your_hand)
 
         if self.sum_of_hand >= 21:
             threading.Thread(target=self.check_who_won).start()
-        elif self.sum_of_hand > self.sum_dealer > 16:
+        elif self.sum_of_hand > self.sum_opponent > 16:
             threading.Thread(target=self.stand).start()
 
+    @mainthread
     def stand(self):
-        while self.sum_dealer < 17:
-            if self.sum_dealer < self.sum_of_hand:
+        while self.sum_opponent < 17:
+            if self.sum_opponent < self.sum_of_hand:
                 SoundLoader.load("sounds/deal_card.wav").play()
-                self.dealer_hand += " " + self.deck[0]
+                self.opponent_hand.append(self.deck[0])
+                self.opponent_card_box.add_widget(
+                    Image(
+                        source=f"images/cards/{self.deck[0]}.jpg",
+                        size_hint_x=None,
+                        width=100
+                    )
+                )
                 del self.deck[0]  # delete "first" card of the deck
-                self.sum_dealer = self.total_sum(self.sum_dealer, self.d_hand, self.dealer_hand)
+                self.sum_opponent = self.total_sum(self.opponent_hand)
                 sleep(1)
             else:
                 break
 
-        if self.sum_of_hand < self.sum_dealer < 22:
+        if self.sum_of_hand < self.sum_opponent < 22:
             self.status_text = "You LOST " + str(self.your_bet) + f" {self.CURRENCY}!"
         else:
             self.status_text = "You WON " + str(self.your_bet * 2) + f" {self.CURRENCY}!"
@@ -165,15 +229,17 @@ class PlayGame(Screen):
 
         self.popup(self.status_text)
 
-    def reset(self):  # function called in popup function
+    def reset(self, popup):  # function called in popup function
+        popup.dismiss()
+        for card_box in [self.my_card_box, self.opponent_card_box]:
+            card_box.clear_widgets()
+
         self.your_bet = 0
         self.sum_of_hand = 0
-        self.your_hand = ""
-        self.y_hand = []
+        self.your_hand = []
 
-        self.sum_dealer = 0
-        self.d_hand = []
-        self.dealer_hand = ""
+        self.sum_opponent = 0
+        self.opponent_hand = []
 
         self.ids.twenty.disabled = False  # enable 20 chip button
         self.ids.fifty.disabled = False  # enable 50 chip button
