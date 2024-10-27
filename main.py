14a3583f590c87b110e95bfa508c17a96b6b0258
rkanapka <kanapka.rimvydas@gmail.com>
2024-10-27 14:10:53 +0200

    ref: move window manager to class

--- main.py
+++ main.py
@@ -8,30 +8,26 @@ from screens.main_menu import MainMenu
 from screens.active_game import ActiveGame
 
 
+kv = Builder.load_file("main.kv")
+
+
 class ImageButton(ButtonBehavior, Image):  # class for making clickable image
     pass
-
 
 class WindowManager(ScreenManager):
     pass
 
-
-kv = Builder.load_file("main.kv")
-
-sm = WindowManager(transition=FadeTransition())
-
-screens = [MainMenu(name="main"), ActiveGame(name="playing")]
-for screen in screens:
-    sm.add_widget(screen)
-
-sm.current = "main"
-
-
 class CyberJack(App):
     def build(self):
         self.icon = 'images/chip.ico'
-        return sm
+        wm = WindowManager(transition=FadeTransition())
 
+        screens = [MainMenu(name="main"), ActiveGame(name="playing")]
+        for screen in screens:
+            wm.add_widget(screen)
+
+        wm.current = "main"
+        return wm
 
 if __name__ == "__main__":
     CyberJack().run()
