e26584355f74680b14adf0cb3be4c6582698a393
rkanapka <kanapka.rimvydas@gmail.com>
2024-10-27 14:42:17 +0200

    imp: play song on active game screen

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -8,3 +8,5 @@ class MainMenu(Screen):
 
         self.main_menu_sound = SoundLoader.load('sounds/main_menu.mp3')
         self.main_menu_sound.play()
+
+        self.active_game_sound = SoundLoader.load('sounds/hyper-spoiler_cyberpunk_2077_ost.mp3')
